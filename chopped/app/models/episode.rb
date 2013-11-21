1c4dc26bc5ae78b616e78a746a3f6a6720f3b376
Raleigh D Froeber <RDFroeber@gmail.com>
2013-11-21 12:11:04 -0500

    Episode testing abstracted to new Producer class

--- chopped/app/models/episode.rb
+++ chopped/app/models/episode.rb
@@ -4,43 +4,12 @@ class Episode < ActiveRecord::Base
   has_many :rounds
   has_many :judges
 
-  after_save :check_rounds, :check_judges
+  after_save :produce_episode
 
-  def check_rounds
-    if self.rounds.count == 1 || self.rounds.count == 2
-      raise "Stop that. You must have three rounds."
-    elsif self.rounds.count == 3 
-      raise "You already have three rounds."
-    elsif self.rounds.count > 3   
-      raise "You cannot have more than three rounds."
-    else
-      create_rounds
-    end
-  end
 
-  def check_judges
-    if self.judges.count == 1 || self.judges.count == 2
-      raise "Stop that. You must have three judges."
-    elsif self.judges.count == 3 
-      raise "You already have three judges."
-    elsif self.judges.count > 3   
-      raise "You cannot have more than three judges."
-    else
-      create_judges
-    end
-  end
-
-  private
-  
-  def create_rounds
-    self.rounds << Round.create(ingredient: "Paprika", episode: self)
-    self.rounds << Round.create(ingredient: "Alligator", episode: self)
-    self.rounds << Round.create(ingredient: "Mint", episode: self)
-  end
-
-  def create_judges
-    self.judges << Judge.create(name: "Alfredo", episode: self)
-    self.judges << Judge.create(name: "Marinara", episode: self)
-    self.judges << Judge.create(name: "Vodka", episode: self)
+  def produce_episode 
+    fabio = Producer.new(self)
+    fabio.research_judges
+    fabio.brainstorm_rounds
   end
 end
